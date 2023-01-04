e36e89c7d35bde66f9f6fee67f10f9f7941cbd3a
maksymvovk <makswowk3d@gmail.com>
2023-01-04 14:37:47 +0200

    Implement RecyclerView in MainActivity

--- app/src/main/java/com/example/shoppingapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/presentation/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.shoppingapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppingapp.R
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var shopViewModel: MainShopViewModel
+    private lateinit var shopItemsAdapter: ShopItemsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpRecyclerView()
         shopViewModel = ViewModelProvider(this)[MainShopViewModel::class.java]
+        shopViewModel.shopListLiveData.observe(this) {
+            shopItemsAdapter.shopItemList = it
+        }
+    }
 
+    private fun setUpRecyclerView() {
+        shopItemsAdapter = ShopItemsAdapter()
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewShopList)
+        recyclerView.adapter = shopItemsAdapter
     }
 }
