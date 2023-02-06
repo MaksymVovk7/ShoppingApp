67b6d77525026557101617561391075415daae9b
maksymvovk <makswowk3d@gmail.com>
2023-02-06 15:38:17 +0200

    Implement ShopItemActivity, shop item layout

---
+++ app/src/main/java/com/example/shoppingapp/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppingapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoppingapp.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
